4e62bbaf0ead394178a32fdaf391d8669cbdbe48
ivan <ivanfan@connect.hku.hk>
2025-08-07 15:35:24 -0700

    add api example

---
+++ main.js
@@ -0,0 +1,33 @@
+import { Client } from "@langchain/langgraph-sdk";
+
+const client = new Client({
+  "apiUrl": "http://10.147.19.97:8000"
+});
+
+// List all assistants
+const assistants = await client.assistants.search({
+  metadata: null,
+  offset: 0,
+  limit: 10,
+});
+
+// We auto-create an assistant for each graph you register in config.
+const agent = assistants[0];
+
+// Start a new thread
+const thread = await client.threads.create();
+
+// Start a streaming run
+const streamResponse = client.runs.stream(
+  thread["thread_id"],
+  agent["assistant_id"],
+  {
+    input: { 
+      "question": "hello"
+     },
+  }
+);
+
+for await (const chunk of streamResponse) {
+  console.log(chunk);
+}
